df0c06acb35b6c688c02de8bbaf0b16e28bd7457
apo-inoue <takeshi.inoue@stransa.co.jp>
2020-08-15 17:26:24 +0900

    feat: hasura setup

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Navigation from './src/navigation/Navigation';
-import { AppRegistry } from 'react-native';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { ResetStyle } from './src/components/theme/ResetStyle';
 import { CustomThemeProvider } from './src/components/theme/CustomThemeProvider';
@@ -9,6 +7,7 @@ import { CustomThemeProvider } from './src/components/theme/CustomThemeProvider'
 // Create the client as outlined in the setup guide
 const client = new ApolloClient({
   cache: new InMemoryCache(),
+  uri: 'https://right-goldfish-91.hasura.app/v1/graphql',
 });
 
 const App = () => {
--- src/page/Today.tsx
+++ src/page/Today.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { ActivityIndicator, FlatList, Text } from 'react-native';
 import { Container, Button } from '../components/ui';
 import { useNavigation } from '@react-navigation/native';
 import { useCustomTheme } from '../components/theme/CustomThemeProvider';
+import { useGetAllTodosQuery } from '../types/graphql';
 
 export const Today = () => {
   const navigation = useNavigation();
   const theme = useCustomTheme();
+  const { loading, error, data } = useGetAllTodosQuery();
+
+  if (loading) return <div />;
+
   return (
     <Container>
+      {console.log(data)}
+      <FlatList
+        data={data.todo}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <Text key={item.id}>{item.title}</Text>}
+      />
       <Button
         onPress={() => navigation.goBack()}
         title="GoHome"
