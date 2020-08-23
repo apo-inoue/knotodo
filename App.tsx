20257e8221481cdc29e760f1c60f14862bc8cd65
apo-inoue <takeshi.inoue@stransa.co.jp>
2020-08-23 15:23:14 +0900

    fix: bug fix apollo

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import Navigation from './src/navigation/Navigation';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
 import { CustomThemeProvider } from './src/containers/theme/provider';
 import { ComposedProviders } from './src/containers/ComposedProviders';
 import { useAuthContext } from './src/containers/auth/useCtx';
 import { LogIn } from './src/page';
 
 // Create the client as outlined in the setup guide
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: 'https://right-goldfish-91.hasura.app/v1/graphql',
-});
 
 const ApolloHandler = () => {
   const {
     state: { token },
   } = useAuthContext();
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+      uri: 'https://hasura.io/learn/graphql',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }),
+  });
 
   if (!token) {
     <LogIn />;
