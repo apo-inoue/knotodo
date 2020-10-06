fb1f8dafadcfbf4b9a82d36f364595e543ed8296
takeshi <62988272+apo-inoue@users.noreply.github.com>
2020-10-06 23:57:37 +0900

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_CLIENT_ID = 'OZw0caLilg38KEnAX87dAgwWDSovaN1q';
-export const AUTH_DOMAIN = 'https://dev-28i2mygc.au.auth0.com';
-export const ID_TOKEN_KEY = 'todo-id-token';
-export const NONCE_KEY = 'todo-nonce';
-export const AUTH_NAMESPACE = 'https://hasura.io/jwt/claims';
